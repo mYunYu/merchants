2d81cd86dd754823da59bc48812711df4734d33b
mYunYu <1577979477@qq.com>
2020-02-14 15:37:54 +0800

    商户投放子系统初始化

---
+++ src/main/java/com/jju/passbook/constant/Constants.java
@@ -0,0 +1,17 @@
+package com.jju.passbook.constant;
+
+/**
+ *  通用常量定义
+ */
+public class Constants {
+
+    // 商户优惠券投放的 Kafka Topic
+    public static final String TEMPLATE_TOPIC = "merchants-template";
+
+    // 通用的token
+    public static final String TOKEN_STRING = "token";
+
+    // token info
+    public static final String TOKEN = "passbook-merchants";
+
+}
---
+++ src/main/java/com/jju/passbook/constant/TemplateColor.java
@@ -0,0 +1,30 @@
+package com.jju.passbook.constant;
+
+/**
+ *  优惠券背景颜色
+ */
+public enum TemplateColor {
+
+    RED(1, "红色"),
+    GREEN(2, "绿色"),
+    BLUE(3, "蓝色");
+
+    //背景颜色代码
+    private Integer code;
+
+    //颜色描述
+    private String color;
+
+    TemplateColor(Integer code, String color){
+        this.code = code;
+        this.color = color;
+    }
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public String getColor() {
+        return color;
+    }
+}
