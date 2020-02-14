ac6b7d77f4fed91aeeb2936ddd5673623a7e528d
mYunYu <1577979477@qq.com>
2020-02-14 16:40:20 +0800

    权限校验+实体对象和dao创建完成

---
+++ src/main/java/com/jju/passbook/dao/MerchantsDao.java
@@ -0,0 +1,25 @@
+package com.jju.passbook.dao;
+
+import com.jju.passbook.entity.Merchants;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ *  Merchants Dao 接口
+ */
+public interface MerchantsDao extends JpaRepository<Merchants, Integer> {
+
+    /**
+     *  根据主键id获取商户对象
+     * @param id    商户 id
+     * @return      Merchants
+     */
+    Merchants findById(Integer id);
+
+    /**
+     *  根据商户名称获取商户对象
+     * @param name  商户名称
+     * @return      Merchants
+     */
+    Merchants findByName(String name);
+
+}
---
+++ src/main/java/com/jju/passbook/security/AccessContext.java
@@ -0,0 +1,22 @@
+package com.jju.passbook.security;
+
+/**
+ *  使用 ThreadLocal 去单独存储每一个线程携带的 Token 信息
+ */
+public class AccessContext {
+
+    private static final ThreadLocal<String> token = new ThreadLocal<>();
+
+    public static String getToken(){
+        return token.get();
+    }
+
+    public static void setToken(String tokenStr){
+        token.set(tokenStr);
+    }
+
+    public static void clearAccessKey(){
+        token.remove();
+    }
+
+}
